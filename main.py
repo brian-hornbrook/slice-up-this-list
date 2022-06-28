e05c1b9ea58b33c0c30462df2686271b2e4eae66
brian-hornbrook <brian_hornbrook@outlook.com>
2022-06-28 13:03:08 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# a list of my friends
+my_friends = [
+    "Foxy", "Devany", "Kaylee", "Ashley", "Maya", "Mia", "Lucky", "Fluffy",
+    "Randal", "Matt", "Chris", "Brian", "Elisa", "Sofia", "Merari", "Buddy",
+    "Ronny", "Carloitos", "Francisco", "David"
+]
+
+# invite first 4 friends out to dinner
+dinner_fiends = str(my_friends[:5])
+print("My dinner friends: " + dinner_fiends)
+
+# invite last 4 friends out for coffee
+coffee_friends = str(my_friends[16:])
+print("My coffee friends: " + coffee_friends)
+
+# invite 4 friends out to hike
+hiking_friends = str(my_friends[8:12])
+print("My hiking friends: " + hiking_friends)
+
+# inite best friend to go for a morning walk
+walk_friend = str(my_friends[14])
+print("My morning walk friend: " + walk_friend)
